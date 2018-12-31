b4f48c54c15f0864297f04619c3b416cd7f630e2
朱文强 <zhuwenqiang@zhuwenqiangdeMacBook-Pro.local>
2018-12-31 12:24:52 +0800

    first commit

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,10 +1,10 @@
 //index.js
 //获取应用实例
 const app = getApp()
-
+//函数用来注册一个页面。接受一个 Object 类型参数，其指定页面的初始数据、生命周期回调、事件处理函数等。
 Page({
   data: {
-    motto: 'Hello World',
+    motto: '123321',
     userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
